0f5ab419abd6f19ec4325e489b4f2be2c7c8b5b8
Your Name <you@example.com>
2025-11-18 08:41:08 +0000

    fin backend

--- scripts/install_lamp_backend.sh
+++ scripts/install_lamp_backend.sh
@@ -2,7 +2,13 @@
 set -ex
 #Actualizamos los repositorios
 apt update
+#Importamos variables
+source .env
 #Actualizamos los paquetes del sistema
 apt upgrade -y
 #Instalación de mysql server
 sudo apt install mysql-server -y
+#Modificamos el parametro bind-address
+sed -i "s/127.0.0.1/$MYSQL_SERVER_PRIVATE_IP/" /etc/mysql/mysql.conf.d/mysqld.cnf
+#Reiniciamos el servicio de mysql
+systemctl restart mysql
