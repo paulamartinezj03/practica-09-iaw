0bde74ef68679c7cb18527422932d20455b7aefb
Your Name <you@example.com>
2025-11-18 08:46:45 +0000

    cambios finales

--- scripts/install_lamp_backend.sh
+++ scripts/install_lamp_backend.sh
@@ -12,3 +12,12 @@ sudo apt install mysql-server -y
 sed -i "s/127.0.0.1/$MYSQL_SERVER_PRIVATE_IP/" /etc/mysql/mysql.conf.d/mysqld.cnf
 #Reiniciamos el servicio de mysql
 systemctl restart mysql
+  #Creamos una base de datos de ejemplo
+mysql -u root -e "DROP DATABASE IF EXISTS $DB_NAME"
+mysql -u root -e "CREATE DATABASE $DB_NAME";
+
+#Creamos un usuario y contraseña para la base de datos
+mysql -u root -e "DROP USER IF EXISTS '$DB_USER'@'$IP_CLIENTE_MYSQL';";
+mysql -u root -e "CREATE USER $DB_USER@'$IP_CLIENTE_MYSQL' IDENTIFIED BY '$DB_PASSWORD'";
+#Le asignamos privilegios de nuestra base de datos
+mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'$IP_CLIENTE_MYSQL'";
